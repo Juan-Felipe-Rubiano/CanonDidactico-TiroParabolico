0129d7b8e9579809cb0dd9d95b03a6ee27decff1
juanfe <juanfeliperzmcs@gmail.com>
2025-10-30 21:45:56 -0500

    init

---
+++ src/main/java/edu/javeriana/juanfe/backfisica/BackFisicaApplication.java
@@ -0,0 +1,13 @@
+package edu.javeriana.juanfe.backfisica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFisicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackFisicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/javeriana/juanfe/backfisica/BackFisicaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.javeriana.juanfe.backfisica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFisicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
